b62767214a5a3f2137a5847b31b05005bbaa0566
Anna Valkov <anna.a.valkov@gmail.com>
2014-07-28 19:02:37 -0700

    can take multiple scores and return multiple types of allergies.

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -2,15 +2,15 @@ def allergies(score)
 	score = score.to_i
 	allergies = {128 => "cats", 64 => "pollen", 32 => "chocolcate", 16 => "tomatoes",
 							 8 => "strawberries",4 => "shellfish", 2 => "peanuts", 1 => "eggs"}
-	type_of_allergy =[]
+	result =[]
 	0.upto(score) do
 		allergies.each do |number, allergy|	
 			if score >= number
-				type_of_allergy << allergy 
+				result << allergy 
 				score -= number
 				break
 			end	
 		end	
 	end	
-	type_of_allergy	
+	result
 end
--- spec/allergies_spec.rb
+++ spec/allergies_spec.rb
@@ -5,7 +5,10 @@ describe('allergies') do
   it("takes the allergy score and returns one allergy") do
     allergies(4).should(eq(["shellfish"]))
   end
-  it("takes the allergy score and returns multiple allergies") do
+  it("takes the allergy score and returns more than one allergy") do
   	allergies(12).should(eq(["strawberries", "shellfish"]))
+  end
+  it("takes the allergy score and returns multiple allergies") do
+  	allergies(28).should(eq(["tomatoes","strawberries", "shellfish"]))
   end
 end

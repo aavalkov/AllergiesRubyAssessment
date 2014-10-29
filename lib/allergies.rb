a7233c7acfa0dc22d946872042045952e08da533
Anna Valkov <anna.a.valkov@gmail.com>
2014-10-28 21:22:00 -0700

    Update allergies.rb

--- lib/allergies.rb
+++ lib/allergies.rb
@@ -1,16 +1,16 @@
 def allergies(score)
-	score = score.to_i
-	allergies = {128 => "cats", 64 => "pollen", 32 => "chocolcate", 16 => "tomatoes",
-							 8 => "strawberries",4 => "shellfish", 2 => "peanuts", 1 => "eggs"}
-	result =[]
-	0.upto(score) do
-		allergies.each do |number, allergy|	
-			if score >= number
-				result << allergy 
-				score -= number
-				break
-			end	
-		end	
-	end	
-	result
+  score = score.to_i
+  allergies = {128 => "cats", 64 => "pollen", 32 => "chocolcate", 16 => "tomatoes",
+		8 => "strawberries",4 => "shellfish", 2 => "peanuts", 1 => "eggs"}
+  result =[]
+  0.upto(score) do
+    allergies.each do |number, allergy|	
+      if score >= number
+        result << allergy 
+	score -= number
+	break
+      end	
+    end	
+  end	
+  result
 end
